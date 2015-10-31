a6fc1535ec1f29918c9346cc98ec56c904f2cd3f
Chris Taylor <christhebaron@gmail.com>
2015-10-31 22:09:42 +0000

    Removes node js server as it is no longer needed

--- index.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var app = express();
-var http = require('http').Server(app);
-
-app.use('/', express.static(__dirname + '/public'));
-
-http.listen(8000, function () {
-	console.log('listening on *:8000');
-});
