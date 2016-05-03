9566eabe986d9f16e2cd2d91ce420cd8ab2fe663
BrookeH <Brooke@nas10-240-74-200.york.ac.uk>
2016-05-03 23:36:44 +0100

    Fixed silence alert setting breaking news to false

--- src/scripts/timelord.js
+++ src/scripts/timelord.js
@@ -318,7 +318,7 @@ window.Timelord = {
 		if (time >= Timelord._config.silence_timeouts.long) {
 			Timelord.setBreakingNews("RADIO SILENCE DETECTED");
 		} else {
-			Timelord.setBreakingNews(false);
+			Timelord.updateBreakingNews();
 		}
 
 		if (time >= Timelord._config.silence_timeouts.short) {
