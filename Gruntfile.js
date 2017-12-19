f691e57e3dccafa9d60ef67f7cf657c68c1747e3
Matthew Stratford <matthew@mstratford.net>
2017-12-19 22:38:08 +0000

    Removed a wiredep comment.

--- Gruntfile.js
+++ Gruntfile.js
@@ -118,7 +118,6 @@ module.exports = function (grunt) {
 		}
 	});
 
-	//grunt.loadNpmTasks('grunt-wiredep');
 	grunt.loadNpmTasks('grunt-sass');
 	grunt.loadNpmTasks('grunt-contrib-connect');
 	grunt.loadNpmTasks('grunt-contrib-watch');
